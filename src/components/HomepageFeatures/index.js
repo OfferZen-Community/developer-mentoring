b54a464797062232be7e3d94e8468f3ef8bb8b6c
Stephen van der Heijden <stephengrantvdh@gmail.com>
2022-04-29 11:38:10 +0200

    Add a smiley face to be happy

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -4,7 +4,7 @@ import styles from './styles.module.css';
 
 const FeatureList = [
   {
-    title: 'Maintained by active dev mentors and mentees',
+    title: 'Maintained by active dev mentors and mentees :)',
     Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
     description: (
       <>
