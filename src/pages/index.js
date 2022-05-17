690bfda0a20c24e52aa952c8d6346a6121310440
OfferZenThrive <andrea@offerzen.com>
2022-05-17 11:25:03 +0200

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,7 +16,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/introduction/01-welcome">
+            to="/docs/introduction/welcome">
             Get started
           </Link>
         </div>
