d2767aa8c1396656eaac4ece4aa61e1a873cb2e9
OfferZenThrive <andrea@offerzen.com>
2022-05-17 11:19:26 +0200

    Changes title of Docusaurus site
    Changes title of Docusaurus site from handbook to guide.

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,7 +6,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'The Developer Mentoring Handbook',
+  title: 'The Developer Mentoring Guide',
   tagline: 'Helping devs help devs',
   url: 'https://developer-mentoring.vercel.app/',
   baseUrl: '/',
@@ -14,7 +14,7 @@ const config = {
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
   organizationName: 'offerzen', // Usually your GitHub org/user name.
-  projectName: 'tech-mentoring-handbook', // Usually your repo name.
+  projectName: 'developer-mentoring-guide', // Usually your repo name.
 
   plugins: [
     [
@@ -50,7 +50,7 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       navbar: {
-        title: 'Dev Mentoring Handbook',
+        title: 'Developer Mentoring Guide',
         logo: {
           alt: 'OfferZen Logo',
           src: 'img/logo.png',
@@ -58,7 +58,7 @@ const config = {
         items: [
           {
             type: 'doc',
-            docId: 'introduction/01-welcome',
+            docId: 'introduction/welcome',
             position: 'left',
             label: 'The Guide',
           },
@@ -78,7 +78,7 @@ const config = {
             items: [
               {
                 label: 'The Guide',
-                to: '/docs/introduction/01-welcome',
+                to: '/docs/introduction/welcome',
               },
             ],
           },
